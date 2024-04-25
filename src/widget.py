0fbb6607c99afaf65f3181563ee9d04e725be901
Женя <zhenia_2007@inbox.ru>
2024-04-25 10:42:44 +0300

    spring 9.2_2

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,14 @@
-from src.masks import card_number, account_number
+from src.masks import account_number, card_number
 
 
 def cart_or_check(user1: str) -> str:
     """указывает какую функцию надо использовать cart или check и выполняет ее"""
 
     if user1[0:4] == "Счет":
-        print("Счет " + account_number(user1[-16:]))
+        return "Счет " + account_number(user1[-16:])
 
     else:
-        print(user1[:-16] + card_number(user1[-16:]))
+        return user1[:-16] + card_number(user1[-16:])
 
 
 def time_formator(time: str) -> str:
