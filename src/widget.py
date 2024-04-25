285b39b89da0240fc81fdc66b60a2d9bc976965d
Женя <zhenia_2007@inbox.ru>
2024-04-25 10:56:28 +0300

    spring 9.2_2

--- src/widget.py
+++ src/widget.py
@@ -12,5 +12,5 @@ def cart_or_check(user1: str) -> str:
 
 
 def time_formator(time: str) -> str:
-    """форматирует время в формате 11.07.2018"""
+    """форматирует время в формате  11.07.2018"""
     return time[8:10] + "." + time[5:7] + "." + time[0:4]
