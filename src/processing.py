83cbbea4e373d52fe5a76b481e6f12a8fafc2f09
Shadring <139446729+Shadring@users.noreply.github.com>
2024-05-20 18:37:47 +0300

    Add files via upload

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+
+def state_checker(input: dict, correct: str) -> dict:
+    """возрашает список только тех словарей в которых есть второе слово"""
+    a = []
+    for i in input:
+        if i["state"] == correct:
+            a.append(i)
+    return a
+
+
+def sort_data(input: list, valu: str) -> list:
+    """сортирует и возрашает список словарей по возрастание либо по убиванию взамисимости от второго слово up либо down"""
+    if valu == "down":
+        sorted_data = sorted(input, key=lambda x: (x['date']))
+    if valu == "up":
+        sorted_data = sorted(input, key=lambda x: (x['date']), reverse=True)
+    return sorted_data
